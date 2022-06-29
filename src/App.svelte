e41618cf73d0a0302539b5801e294d0c008f2b02
Brenofaria <brenofariac@gmail.com>
2022-06-29 04:26:01 -0400

    service worker and manifest added

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
 	import Layout from "./__layout.svelte";
+
+	if ('serviceWorker' in navigator)
+	{
+		navigator.serviceWorker.register('/service-worker.js')
+	}
 </script>
+<link rel="apple-touch-icon" href="/mainicon.png">
+<meta name="theme-color" content="#a81d4d">
 
 <Layout />
